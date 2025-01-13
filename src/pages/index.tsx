caf31379b95d46cf5b32e11d48036ea82db939fc
Jimmy <jimmy@commissaryconnect.com>
2025-01-13 09:23:33 -0800

    fixed favicon, added content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,19 @@ export default function Home() {
         <meta name="description" content="Welcome to my portfolio! I'm Jimmy, a Software Developer and UX/UI Designer based in Toronto, Canada
         with a strong background software development and UX/UI design." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <HeaderNav/>
+      <main className={styles.page}>
+        <div className={styles.centerContent}>
+          <h1 className={styles.name}>JIMMY LAM</h1>
+          <div className={styles.details}>
+            <p className={styles.role}>Software Developer</p>
+            <p className={styles.location}>Based in <span className={styles.city}>TORONTO</span></p>
+            <p className={styles.company}>Currently @ Commissary Connect</p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
